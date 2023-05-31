754067be0b3028ba3bfc20fa66e2b0adfc62fb03
Leonard Mercedes <100613411+LMercedes03@users.noreply.github.com>
2023-05-31 17:56:27 -0400

    Work on ex2

--- src/Pages/FoodPage.js
+++ src/Pages/FoodPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'react-bootstrap/Image'
 import './FoodPage.scss'
 
 export const FoodPage = () => {
@@ -8,13 +9,21 @@ export const FoodPage = () => {
           <div className='FoodPage_Header'>
                 Food 
             </div>
-            <div>
-                <br/>
-                Todo...
-                <br/>
-                <br/>
-            </div>
-       </div>
+            <div className='FoodPage_Content'>
+                    <FoodTable />
+                </div>
+                <div className='FoodPage_Content'>
+                    <div className='FoodPage_Pix'>
+                        <Image width='800px' rounded={true} fluid={true} src='croissant.jpg' alt='croissant' />
+                        <br /><br />
+                        <Image width='800px' rounded={true} fluid={true} src='muffin.jpg' alt='muffin' />
+                        <br /><br />
+                        <Image width='800px' rounded={true} fluid={true} src='cookie.jpg' alt='cookie' />
+                        <br /><br />
+                        <Image width='800px' rounded={true} fluid={true} src='macaron.jpg' alt='macaron' />
+                    </div>
+              </div>
+        </div>
     </div>
   )
 }
